8190f3e3df774cfb84ae06cb9a252b2e3cbe5b38
Mia-code-1628 <miatorres1628@gmail.com>
2024-08-29 19:33:59 -0300

    Update 25_03.py

--- 25_03.py
+++ 25_03.py
@@ -1,18 +1,16 @@
-# Estructuras de datos
-numero = int(input("Ingrese un número entero para la tabla de multiplicar: "))  # Entrada de datos - Estructura de datos
-tabla_multiplicar = []  # Creación de una lista vacía - Estructura de datos
+numero = int(input("Ingrese un número entero para la tabla de multiplicar: "))  
+tabla_multiplicar = []
 
-for i in range(1, 11):  # Estructura de control de programación
-    resultado = numero * i  # Operación matemática - Programación
-    tabla_multiplicar.append(resultado)  # Agregar resultado a la lista - Estructura de datos
+for i in range(1, 11): 
+    resultado = numero * i 
+    tabla_multiplicar.append(resultado) 
 
-print("Tabla de multiplicar de", numero, ":")  # Salida de texto - Programación
-for j in range(10):  # Estructura de control de programación
-    print(numero, "x", j+1, "=", tabla_multiplicar[j])  # Salida de texto y elementos de la lista - Programación y estructura de datos
+print("Tabla de multiplicar de", numero, ":") 
+for m in range(10): 
+    print(numero, "x", m+1, "=", tabla_multiplicar[m])
 
-# Estructuras de control de programación
-numero_dividir = int(input("Ingrese un número entero para la tabla de dividir: "))  # Entrada de datos - Estructura de datos
-print("Tabla de dividir del número", numero_dividir, "de mayor a menor:")  # Salida de texto - Programación
-for k in range(10, 0, -1):  # Estructura de control de programación
-    resultado_division = numero_dividir / k  # Operación matemática - Programación
-    print(numero_dividir, "/", k, "=", resultado_division)  # Salida de texto y resultado de la división - Programación
+numero_dividir = int(input("Ingrese un número entero para la tabla de dividir: ")) 
+print("Tabla de dividir del número", numero_dividir, "de mayor a menor:") 
+for t in range(10, 0, -1):  
+    resultado_division = numero_dividir / t
+    print(numero_dividir, "/", t, "=", resultado_division) 
