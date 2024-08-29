77a2dda8321d1bda023e2ab98f0e6b1f39e54d87
Mia-code-1628 <miatorres1628@gmail.com>
2024-08-29 19:37:42 -0300

    Update 25_03.py

--- 25_03.py
+++ 25_03.py
@@ -1,16 +1,13 @@
-numero = int(input("Ingrese un número entero para la tabla de multiplicar: "))  
-tabla_multiplicar = []
+numero = int(input("Ingrese un numero entero: "))#Pide al usuario que ingrese un numero
 
-for i in range(1, 11): 
-    resultado = numero * i 
-    tabla_multiplicar.append(resultado) 
+print("Tabla de multiplicar de", numero)
+for i in range(1, 11):  #Bucle for del 1 al 10.
+    resultado = numero * i  #Multiplica el número ingresado por el índice del bucle.
+    print(numero, "x", i, "=", resultado)  #Muestra la multiplicación en consola.
 
-print("Tabla de multiplicar de", numero, ":") 
-for m in range(10): 
-    print(numero, "x", m+1, "=", tabla_multiplicar[m])
+numero2 = int(input("Ingrese otro número entero: "))  #Lee la entrada del usuario y la convierte a entero.
 
-numero_dividir = int(input("Ingrese un número entero para la tabla de dividir: ")) 
-print("Tabla de dividir del número", numero_dividir, "de mayor a menor:") 
-for t in range(10, 0, -1):  
-    resultado_division = numero_dividir / t
-    print(numero_dividir, "/", t, "=", resultado_division) 
+print("Tabla de dividir de", numero2, "en orden descendente")
+for m in range(10, 0, -1):  #Bucle for para iterar de 10 a 1 en orden descendente.
+    resultado = numero2 / m  #Divide el segundo número ingresado por el índice del bucle.
+    print(numero2, "÷", m, "=", resultado) #Muestra el resultado
